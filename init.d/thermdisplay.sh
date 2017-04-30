61d346b547206c84507c326c58e1296be641920f
thompsonline <john.thompson@start-thinking.com>
2017-04-30 15:16:18 -0600

    Changed location of the files. Updated setup instructions

--- init.d/thermdisplay.sh
+++ init.d/thermdisplay.sh
@@ -11,7 +11,7 @@
 ### END INIT INFO
 
 # Change the next 3 lines to suit where you install your script and what you want to call it
-DIR='/home/john/thermostat-display'
+DIR='/home/john/thermy/thermostat-display'
 DAEMON=$DIR/routes.py
 DAEMON_NAME='thermdisplay'
 LOGFILE=/var/log/thermdisplay.log
