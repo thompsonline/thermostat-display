d0b154ebd182a911515e96f5a4f59a7be28d81ea
thompsonline <john.thompson@start-thinking.com>
2017-06-16 12:33:16 -0600

    Added a timeout to the temp set UI where if after pressing the temp set button there isn't activity for 60 seconds, the operation is cancelled.

--- static/increment.js
+++ static/increment.js
@@ -5,10 +5,12 @@ function decrement(myInput) {
   myInput.value = (myInput.value - 1) || 0;
 }
 function incrementValue(element) {
+  resetTempSetTimeout(); // Start the temp set timeout because we've done something
   element.text(Number(element.text()) + 1);
   if (element.text() > 100) element.text(100);
 }
 function decrementValue(element) {
+  resetTempSetTimeout(); // Start the temp set timeout because we've done something
   element.text(Number(element.text()) - 1);
   if (element.text() < 40) element.text(40);
 }
